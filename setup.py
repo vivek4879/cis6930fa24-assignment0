241a1b709030f55cfeae5ff53a3bc8a0dd22fe7d
Vivek Aher <vivek4879gmail.com>
2024-09-09 22:50:39 -0400

    modified setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 	name='assignment0',
 	version='1.0',
 	author='Vivek Milind Aher',
-	author_email='yvaher@ufl.edu',
+	author_email='vaher@ufl.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
