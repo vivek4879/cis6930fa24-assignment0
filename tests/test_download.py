09ae3486da5858efb6df469fe38e02921f45013c
Vivek Aher <vivek4879gmail.com>
2024-09-09 20:05:10 -0400

    modified test files

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,12 +1,12 @@
 test_data = '{"items": [{"title": "vivek a him", "subjects": ["ads", "dbms", "mis"], "field_offices": ["Pune", "Mumbai", "Nashik"]}, {"title": "vivek a him", "subjects": ["ads", "dbms", "mis"], "field_offices": ["Pune", "Mumbai", "Nashik"]}]}'
+
 import json
-from main import get_data_from_url, extract_title, extract_field_offices, extract_subjects
+from main import get_data_from_url, extract_title, extract_field_offices, extract_subjects, output_print
 from unittest.mock import MagicMock
 
-# Downloading non-empty data from a URL
 
+# Downloading non-empty data from a URL
 def test_Downloading_non_empty_data(mocker):
-    #response = json.loads(test_data)
     mock_response = mocker.MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = test_data
@@ -22,6 +22,8 @@ def test_Extracting_title():
     for i in range(len(response["items"])):
         title = extract_title(response["items"][i])
         assert title == 'vivek a him'
+
+
 # Extrating subjects field from FBI API
 def test_Extracting_field_offcies():
     response = json.loads(test_data)
@@ -37,5 +39,25 @@ def test_Extracting_subjects():
         assert subjects == 'ads,dbms,mis'
 
 # Printing the full thorn separated file.
+def test_printing_thorn_seperated_file(mocker):
+    response = json.loads(test_data)
+    mock_response = mocker.MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = response
+    url = 'https://api.fbi.gov/wanted/v1/list'
+    mocker.patch("requests.get", return_value = mock_response)
+    result = get_data_from_url(url, 2)
+    # Expected printed output
+    expected_output = ["vivek a himþads,dbms,misþPune,Mumbai,Nashik","vivek a himþads,dbms,misþPune,Mumbai,Nashik"]
+    # Mocking the print function
+    mock_print = mocker.patch('builtins.print')
+    # Call the function
+    output_print(result)
+    # Check if the print function was called with the correct output
+    printed_calls = [call.args[0] for call in mock_print.call_args_list]  # Extract printed strings
+    assert printed_calls == expected_output
+
+
+
 
 
